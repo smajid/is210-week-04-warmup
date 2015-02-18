5cba9f1ca61bb8c2ac3350d81719d2d6bbd2b097
Saulat Majid <saulat.majid@spsmail.cuny.edu>
2015-02-18 21:51:06 +0000

    raw input function files.

---
+++ task_01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A file that will use raw input function."""
+
+
+HOMEWORK_DONE = raw_input('Have you finished your assignment? ')
+ANSWER = '{}'
+MY_ANSWER = ANSWER.format(HOMEWORK_DONE)
+print MY_ANSWER
---
+++ task_02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Second file to use raw input function."""
+
+
+MY_QUESTION = raw_input('How many colors are in a rainbow? ')
+MY_INTEGER = '7'
+MY_INTEGER = int(MY_INTEGER)
+print MY_INTEGER
